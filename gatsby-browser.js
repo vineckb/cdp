6e9f10dd63447b986e30fd2cea74495e983ad1d1
Vinicius Siuta <vinisiuta@gmail.com>
2020-09-25 23:27:33 -0300

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Layout from './src/components/Layout'
-import './src/reset.css'
-
-export function wrapPageElement({ element, props }) {
-  return <Layout {...props}>{element}</Layout>
-}
